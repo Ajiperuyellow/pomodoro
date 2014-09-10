3b29beca38736243c98d43f7025787cb15dfad10
Moritz <greif@th.physik.uni-frankfurt.de>
2014-09-10 17:49:08 +0200

    All classes so far

--- test.cpp
+++ test.cpp
@@ -7,11 +7,28 @@
 
 
 int add(int i, int j){return i+j;}
-FrameworkTracking aObject;
+FrameworkTracking aFrameworkTrackingObject;
+FrameworkPlanning aFrameworkPlanningObject;
 
 BOOST_AUTO_TEST_CASE(myCaseName)
 {
-
+    //check the testcase
     BOOST_CHECK(add(4,4)==9);
-    BOOST_CHECK(aObject.StartTimer()==6);
+    //check the timer
+    BOOST_CHECK(aFrameworkTrackingObject.StartTimer()==6);
+    //check the timer
+    BOOST_CHECK(aFrameworkTrackingObject.TimerHasRunOut()==6);
+    //check the interrupt
+    BOOST_CHECK(aFrameworkTrackingObject.Interrupt()==6);
+    //check the EnterListItem
+    BOOST_CHECK(aFrameworkPlanningObject.EnterListItem()==6);
+    //check the NewName
+    BOOST_CHECK(aFrameworkPlanningObject.NewNameOfProject()==6);
+    //check the SetNumber
+    BOOST_CHECK(aFrameworkPlanningObject.SetNumberOfPomodori()==6);
+    //check the OpenExisting
+    BOOST_CHECK(aFrameworkPlanningObject.OpenExistingProject()==6);
+    //check the SetTodaysObject
+    BOOST_CHECK(aFrameworkPlanningObject.SetTodaysObjects()==6);
 }
+
