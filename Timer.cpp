a0c439a9df1318fd9d7dfa4635a87d13420adfe1
ThorbenGraf <thorben_graf@gmx.de>
2014-09-11 11:35:10 +0200

    New Timer

--- Timer.cpp
+++ Timer.cpp
@@ -1,49 +1,15 @@
-#include <iostream>
-#include <chrono>
-#include <thread>
 #include "Timer.h"
+#include <iostream>
 
-using namespace std;
-
-Timer::Timer()
-{}
-
-
-int Timer::ElapsedTime(){
-  
-    std::chrono::time_point<std::chrono::system_clock> start, now;
-    start = std::chrono::system_clock::now();
-    std::time_t start_time = std::chrono::system_clock::to_time_t(start);
-    
-    int elapsed_minutes=0;
-    int elapsed_seconds=0;
-    int milliseconds=1000;
-    
-    while (elapsed_minutes<2/*25*/ /*&& elapsed_seconds<0*//*59*/)
-    {
-	cout << elapsed_minutes << endl;
-	
-	std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));		     
-	
-	now = std::chrono::system_clock::now();
-	
-	elapsed_minutes = std::chrono::duration_cast<std::chrono::minutes>
-                             (now-start).count();
-	elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>
-                             (now-start).count();
-	
-	//cout << "noch nicht! ";
-    }
-    
-    return elapsed_minutes;
-    //cout << elapsed_minutes << endl;
-    //cout << elapsed_seconds << endl;
-}
-  
-int main()
+Timer::Timer(QObject *parent) :
+  QObject(parent),
+  t(this)
 {
-    
-  Timer TimerInstance;
-  TimerInstance.ElapsedTime();
-  //cout << ;
+  connect(&t,SIGNAL(timeout()),this,SLOT(Tick()));
+  t.start(1000);
+}
+
+void Timer::Tick()
+{
+  std::cout << "tick" << std::endl;
 }
--- Timer.h
+++ Timer.h
@@ -1,6 +1,21 @@
-class Timer
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <QObject>
+#include <QTimer>
+
+class Timer : public QObject
 {
+  Q_OBJECT
 public:
-  Timer();
-  int ElapsedTime();
+  explicit Timer(QObject *parent = 0);
+
+public slots:
+  void Tick();
+
+private:
+  QTimer t;
+
 };
+
+#endif // FRAMEWORK_H
